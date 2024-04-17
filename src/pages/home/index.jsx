b3d31379b0295de19db59c4f515fa86f1d2ae341
Armen <aremniritsyan995@gmail.com>
2024-04-17 16:51:09 +0400

    lesson-4-Armen

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,34 +1,12 @@
-import { useState } from "react";
-import { ROUTES } from "../../utils/constants";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./style.scss";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
-
-  const goToRegisterPage = () => {
-    navigate(ROUTES.register);
-  };
-
-  const join = () => {
-    const userData = JSON.parse(localStorage.getItem("userData"));
-
-    if (userData && userData.login === login && userData.password === password) {
-      alert("Success, you have logged in!");
-      navigate(ROUTES.home);
-    } else {
-      alert("Error, incorrect login or password.");
-    }
-  };
-
   return (
     <main>
-      <input type="text" value={login} onChange={(e) => setLogin(e.target.value)} placeholder="Login" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-      <button onClick={goToRegisterPage}>Register</button>
-      <button onClick={join}>Join</button>
+      <h1>Добро пожаловать на наш сайт!</h1>
+      <p>Это платформа для обмена знаниями, где пользователи могут задавать вопросы и получать ответы от сообщества. Здесь вы можете найти ответы на свои вопросы, поделиться своими знаниями и узнать что-то новое.</p>
+      <p>Чтобы начать, пожалуйста, <Link to="/register">зарегистрируйтесь</Link> или <Link to="/login">войдите</Link> в свой аккаунт.</p>
       <Link to="https://google.com" target="_blank">google</Link>
     </main>
   );
